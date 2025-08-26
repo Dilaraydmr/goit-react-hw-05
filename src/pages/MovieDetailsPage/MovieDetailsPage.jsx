2583bc0fd89250f81de70e39a397978cf51df7ff
dilaraydmr <dilaraaydemir1@gmail.com>
2025-08-27 00:42:51 +0300

    güncelleme

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,25 @@
-const MovieDetailsPage = () => {
-    return <h1>Movie Details Page</h1>;
-};
+import { useParams, Outlet, Link } from "react-router-dom";
 
-export default MovieDetailsPage;
+function MovieDetails() {
+    const { movieId } = useParams();
+
+    return (
+        <div>
+            <h2>Movie Details Page</h2>
+            <p>Movie ID: {movieId}</p>
+
+            <ul>
+                <li>
+                    <Link to="cast">Cast</Link>
+                </li>
+                <li>
+                    <Link to="reviews">Reviews</Link>
+                </li>
+            </ul>
+
+            <Outlet />
+        </div>
+    );
+}
+
+export default MovieDetails;
---
+++ src/services/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const API_KEY = 'YOUR_TMDB_KEY';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchTrendingMovies = async () => {
+  const resp = await axios.get(`${BASE_URL}/trending/movie/day`, {
+    params: { api_key: API_KEY },
+  });
+  return resp.data.results;
+};
+
+export const fetchSearchMovies = async (query) => {
+  const resp = await axios.get(`${BASE_URL}/search/movie`, {
+    params: { api_key: API_KEY, query },
+  });
+  return resp.data.results;
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}`, {
+    params: { api_key: API_KEY },
+  });
+  return resp.data;
+};
+
+export const fetchMovieCast = async (movieId) => {
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, {
+    params: { api_key: API_KEY },
+  });
+  return resp.data.cast;
+};
+
+export const fetchMovieReviews = async (movieId) => {
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, {
+    params: { api_key: API_KEY },
+  });
+  return resp.data.results;
+};
