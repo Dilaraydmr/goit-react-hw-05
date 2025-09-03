be2fcb54055e0061cbd4f8b90c6e8a687380bee2
dilaraydmr <dilaraaydemir1@gmail.com>
2025-09-04 00:46:47 +0300

    css update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchTrendingMovies } from "../../services/api";
 import { Link } from "react-router-dom";
-import styles from "./HomePage.module.css"; 
+import styles from "./HomePage.module.css";
 
 function HomePage() {
     const [movies, setMovies] = useState([]);
@@ -20,21 +20,28 @@ function HomePage() {
 
     return (
         <div className={styles.container}>
-            <h2 className={styles.title}>Trending Today</h2>
+            <h2 className={styles.title}>🔥 Trending Today</h2>
             <ul className={styles.list}>
                 {movies.map((movie) => (
                     <li key={movie.id} className={styles.item}>
-                        <Link to={`/movies/${movie.id}`}>
+                        <Link to={`/movies/${movie.id}`} className={styles.card}>
                             {movie.poster_path ? (
                                 <img
-                                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                                     alt={movie.title}
                                     className={styles.image}
                                 />
                             ) : (
                                 <div className={styles.placeholder}>No Image</div>
                             )}
-                            <p className={styles.name}>{movie.title}</p>
+                            <div className={styles.info}>
+                                <p className={styles.name}>{movie.title}</p>
+                                <p className={styles.date}>
+                                    {movie.release_date
+                                        ? movie.release_date.slice(0, 4)
+                                        : "N/A"}
+                                </p>
+                            </div>
                         </Link>
                     </li>
                 ))}
